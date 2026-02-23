3fd8d92d65ceb7fec20bf307e727ca3b1b377ec6
Aditya Suresh <adityas.mca25@siescoms.sies.edu.in>
2026-02-23 12:32:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(string[] args){
         System.out.println("Hello world");
         System.out.println("Addition"+(2+3));
+        System.out.println("Making changes");
     }
 }
