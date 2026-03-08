9d621f55b92b058a697c5486557f622f1158057f
Aditya Suresh <adityas.mca25@siescoms.sies.edu.in>
2026-03-08 15:20:29 +0530

    Add a new print statement to Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello{
        int num2 = 10;
        int sum = num1 + num2;
        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+      System.out.println("This is updated line");
    }
 }
